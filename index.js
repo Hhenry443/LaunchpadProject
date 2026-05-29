61e5c1b27a17c024b27e62c291f4993ffbc06d30
Henry Barnes <henrytbarnes07@gmail.com>
2026-05-29 11:44:54 +0100

    Initial Backend

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html", (err) => {
+    if (err) console.log(err);
+  });
+});
+
+app.get("/hello", (req, res) => {
+  res.send("This is the request to Hello!");
+});
+
+app.get("/hello/:name", (req, res) => {
+  const routeParameter = req.params;
+  const name = routeParameter.name;
+  // or const name = req.params.name
+  res.send(`Hello ${name}, welcome to my NodeJS application!`);
+});
+
+app.get("/form", (req, res) => {
+  res.sendFile(__dirname + "/public/form.html", (err) => {
+    if (err) console.log(err);
+  });
+});
+
+app.post("/form", (req, res) => {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+
+  res.send("Hello, " + fname + " " + lname);
+});
+app.listen(port, () => {
+  console.log(`myapp is listening on port ${port}!`);
+});
